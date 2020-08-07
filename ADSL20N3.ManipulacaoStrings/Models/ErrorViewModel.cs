2a651f5a8b0570981ad93af3d9dbfb8ad0f74c78
fdbva <villela.felipe@gmail.com>
2020-08-07 20:48:04 -0300

    Add project files.

---
+++ ADSL20N3.ManipulacaoStrings/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ADSL20N3.ManipulacaoStrings.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
